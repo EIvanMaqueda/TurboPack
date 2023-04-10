f8cccfcb4297823777085d9d33ee292b85017530
EIvanMaqueda <eivanmaqueda@gmail.com>
2023-04-10 12:24:41 -0600

    Add project files.

---
+++ PL/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+
+        
+    }
+}
---
+++ PL/wwwroot/js/usuario.js
@@ -0,0 +1,27 @@
+﻿function Usuario() {
+    $.ajax({
+        url: 'https://randomuser.me/api/',
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+            $('#cardbody');
+            $.each(data.results, function (i, usuario) {
+                var filas =
+                    '<div class="col" >'+
+                    ' <div class="card">'+
+                '<div class="card-img">'+
+                    '<img id="Img" src="' + usuario.picture.medium+'" style="width:90px ; height:100px" />'+
+                '</div>'+
+                    '<span>' + usuario.name.first+" "+ usuario.name.last+'</span>'+
+                    '<p class="card-name">' + usuario.location.country+" "+usuario.location.city+'</p>'+
+            '</div >'+
+                    '</div >';
+                $("#cardbody").append(filas);
+            });
+        }
+    });
+};
+
+function Limpiar() {
+    $('#cardbody').empty();
+}
